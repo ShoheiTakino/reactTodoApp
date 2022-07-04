1c8a19dbe55c7328a6516146d0a5a1072a0b6fea
ShoheiTakino <104742356+ShoheiTakino@users.noreply.github.com>
2022-07-04 20:48:10 +0900

    cssinjs(complete&incomplete)

--- src/components/IncompleteTodos.jsx
+++ src/components/IncompleteTodos.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 
 export const IncompleteTodos = (props) => {
+  const style = {
+    backgroundColor: "#c6ffe2",
+    width: "400px",
+    minHeight: "200px",
+    padding: "8px",
+    margin: "8px",
+    borderRadius: "8px"
+  };
   const { todos, onClickComplete, onClickDelete } = props;
   return (
-    <div className="imcomplete-area">
+    <div style={style}>
       <p className="title">未完了のTODO　</p>
       <ul>
         {todos.map((todo, index) => {
           //複数の配列を代入する際は、kye={todo}で識別できるようにしないと仮想ドムの差分のみ抽出ができなくなる。
           return (
-            <div key={todo} className="list-row">
+            <div key={todo}>
               <li>{todo}</li>
               <button onClick={() => onClickComplete(index)}>完了</button>
               <button onClick={() => onClickDelete(index)}>削除</button>
